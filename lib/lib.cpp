fc629bf99f5f0bbea1a18c57b76a3c1c953635fd
Vasyl Demianov <v.demianov@isrdefence.com>
2024-05-15 15:57:53 +0300

    Basic linking a library to an executable.

---
+++ lib/lib.cpp
@@ -0,0 +1,7 @@
+#include "lib.h"
+#include <iostream>
+
+void say_hello()
+{
+    std::cout << "hello" << std::endl;
+}
---
+++ lib/lib.h
@@ -0,0 +1,6 @@
+#ifndef LIB_H_LASDUJHOSAUFHDSAODUFI
+#define LIB_H_LASDUJHOSAUFHDSAODUFI
+
+void say_hello();
+
+#endif
